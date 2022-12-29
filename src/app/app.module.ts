e1c0babe823cbc2a72c5b5e89721b31bf979c902
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-29 14:35:00 -0700

    Add font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { NavComponent } from './components/nav/nav.component';
 import { PageWrapperComponent } from './components/page-wrapper/page-wrapper.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, PageWrapperComponent, QuotesComponent],
@@ -19,6 +20,7 @@ import { QuotesComponent } from './components/quotes/quotes.component';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  faDumbbell = faDumbbell;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
