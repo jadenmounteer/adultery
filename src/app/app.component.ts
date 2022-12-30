624ecce4a6c203460ac31b45aa35913525bf109e
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-30 09:37:14 -0700

    Connect to realtime database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tubular';
+
+  constructor(private http: HttpClient) {
+    // const testData = {
+    //   title: 'test',
+    //   content: 'hello world',
+    // };
+    // this.onCreatePost(testData);
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    this.http
+      .post(
+        'https://tubular-cccbd-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
 }
