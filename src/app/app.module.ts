c3aa9a00f5854e28552029445be7765d3d399798
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-12-30 08:37:47 -0700

    Remove firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { NavComponent } from './components/nav/nav.component';
 import { PageWrapperComponent } from './components/page-wrapper/page-wrapper.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, PageWrapperComponent, QuotesComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    FontAwesomeModule,
+  declarations: [
+    AppComponent,
+    NavComponent,
+    PageWrapperComponent,
+    QuotesComponent,
   ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
