32bc304598c01559cc8a1c3b6cd2ec78d7a9ba0c
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-01-11 07:30:54 -0700

    Begin renaming script

--- src/scripts/deploy-to-github-pages.sh
+++ src/scripts/deploy-to-github-pages.sh
@@ -1,4 +1,4 @@
 cd /Users/jadenmounteer/git/tubular
 npm i angular-cli-ghpages --save-dev
-ng build --base-href "https://jadenmounteer.github.io/tubular/"
-npx angular-cli-ghpages --dir=dist/tubular
+ng build --base-href "https://jadenmounteer.github.io/adultery/"
+npx angular-cli-ghpages --dir=dist/adultery
