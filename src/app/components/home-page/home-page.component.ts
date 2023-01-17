aa70448b9799b951f46a8e08eb165f3ee29dee84
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-01-17 13:01:02 -0700

    Add more animations

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,9 +7,22 @@ import { AnimationOptions } from 'ngx-lottie';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  public homePageAnimation: AnimationOptions = {
+  public kidAgainAnimation: AnimationOptions = {
     path: 'https://assets1.lottiefiles.com/private_files/lf30_x8jhuhr4.json',
   };
+
+  public fitnessAnimation: AnimationOptions = {
+    path: 'https://assets2.lottiefiles.com/packages/lf20_ju61m9x9.json',
+  };
+
+  public timeManagementAnimation: AnimationOptions = {
+    path: 'https://assets1.lottiefiles.com/packages/lf20_i7ooqm2q.json',
+  };
+
+  public financeAnimation: AnimationOptions = {
+    path: 'https://assets2.lottiefiles.com/packages/lf20_qxdztkhq.json',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
