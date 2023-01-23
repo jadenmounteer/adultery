afbf41ea2bf949b77b6eeddffbf9af1c413d071a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-01-23 07:14:27 -0700

    Refactor animation component

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AnimationOptions } from 'ngx-lottie';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
-  public kidAgainAnimation: AnimationOptions = {
-    path: 'https://assets1.lottiefiles.com/private_files/lf30_x8jhuhr4.json',
-  };
-
-  public fitnessAnimation: AnimationOptions = {
-    path: 'https://assets2.lottiefiles.com/packages/lf20_ju61m9x9.json',
-  };
-
-  public timeManagementAnimation: AnimationOptions = {
-    path: 'https://assets1.lottiefiles.com/packages/lf20_i7ooqm2q.json',
-  };
-
-  public financeAnimation: AnimationOptions = {
-    path: 'https://assets2.lottiefiles.com/packages/lf20_qxdztkhq.json',
-  };
-
+export class HomePageComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
