516bce102e21692b866f4a5a012efa8f6d85a6b5
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-04 07:55:46 -0700

    Add login component functionality

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public onSubmit(form: NgForm) {
+    this.authService.login({
+      email: form.value.email,
+      password: form.value.password,
+    });
   }
-
 }
