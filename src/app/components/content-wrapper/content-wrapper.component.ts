51e5458b31a36031b45f76b35af8b227ade1fe3b
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-07 07:46:20 -0700

    Configure content loaded input

--- src/app/components/content-wrapper/content-wrapper.component.ts
+++ src/app/components/content-wrapper/content-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-wrapper',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-wrapper.component.scss'],
 })
 export class ContentWrapperComponent implements OnInit {
-  public contentLoaded: boolean = false;
+  @Input() contentLoaded: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
