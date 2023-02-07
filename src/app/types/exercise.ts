8fdced8fbb1327a87603319393ec69598fe07ade
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-07 07:11:00 -0700

    Update to Exercise type

--- src/app/types/exercise.ts
+++ src/app/types/exercise.ts
@@ -1,9 +1,12 @@
+import { ExerciseLog } from './exercise-log';
+import { Max } from './max';
+
 export interface Exercise {
   payload: any;
   id: string;
   defaultQuote: boolean;
   exerciseImage: string | null;
   description: string | null;
-  maxes: null;
-  logs: null;
+  maxes: Max[] | null;
+  logs: ExerciseLog[] | null;
 }
