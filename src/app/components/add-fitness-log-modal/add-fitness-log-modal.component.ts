06d3f1fa0e2e6c1c2f2cf139daeeffe1bf202f32
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-13 07:23:17 -0700

    More updates to logging

--- src/app/components/add-fitness-log-modal/add-fitness-log-modal.component.ts
+++ src/app/components/add-fitness-log-modal/add-fitness-log-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Exercise } from 'src/app/types/exercise';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-add-fitness-log-modal',
@@ -10,7 +12,28 @@ import { Exercise } from 'src/app/types/exercise';
 export class AddFitnessLogModalComponent implements OnInit {
   @Input() parentItem!: Exercise;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(
+    public activeModal: NgbActiveModal,
+    public authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
+
+  public onSubmit(form: NgForm) {
+    const logDescription = form.value.logDescription;
+    // const newExercise: Exercise = {
+    //   id: '',
+    //   userId: this.authService.userId,
+    //   defaultExercise: false,
+    //   exerciseImage: null,
+    //   name: form.value.exerciseName,
+    //   description: form.value.exerciseDescription,
+    //   defaultTags: null,
+    // };
+
+    // this.exercisesService.addNewExercise(newExercise);
+    this.activeModal.close('Close click');
+  }
+
+  private getDate() {}
 }
