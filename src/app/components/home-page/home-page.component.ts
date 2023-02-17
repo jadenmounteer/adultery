5a817b8fd0b1b334c075469883b0e909548d3aa7
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-17 07:36:44 -0700

    Add navigation to home buttons

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-  constructor() {}
+  constructor(public router: Router) {}
 }
