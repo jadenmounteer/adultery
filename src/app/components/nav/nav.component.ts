3cc13502fea4ab9bc462cde23ff10ee369ee65b8
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-03-22 06:21:33 -0600

    Add better navbar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IconService } from 'src/app/services/icon.service';
-
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
-  constructor(public icon: IconService) {}
+export class NavComponent implements OnInit, OnDestroy {
+  public isAuth: boolean = false;
+  private authSubscription!: Subscription;
 
-  ngOnInit(): void {}
+  protected isMenuCollapsed: boolean = true;
+
+  constructor(public icon: IconService, public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 }
