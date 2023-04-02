cae0e4176f01b9db1322869722bb5e6167cd8873
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-01 21:47:03 -0600

    More updates to group modal

--- src/app/components/add-group-modal/add-or-edit-group-modal.component.ts
+++ src/app/components/add-group-modal/add-or-edit-group-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Group } from 'src/app/types/group';
@@ -9,21 +9,22 @@ import { Group } from 'src/app/types/group';
   styleUrls: ['./add-or-edit-group-modal.component.scss'],
 })
 export class AddOrEditGroupModalComponent implements OnInit {
-  constructor(public activeModal: NgbActiveModal) {}
+  @Input() groupToEdit!: Group;
+  protected groupModel: Group = {
+    id: '',
+    groupName: null,
+    users: [],
+  };
+
+  constructor(public activeModal: NgbActiveModal) {
+    if (this.groupToEdit) {
+      this.groupModel = { ...this.groupToEdit };
+    }
+  }
 
   ngOnInit(): void {}
 
   public onSubmit(form: NgForm) {
-    // const group: Group = {
-    //   id: '',
-    //   userId: this.authService.userId,
-    //   defaultExercise: false,
-    //   exerciseImage: null,
-    //   name: form.value.exerciseName,
-    //   description: form.value.exerciseDescription,
-    //   defaultTags: null,
-    // };
-
     // this.exercisesService.addNewExercise(newExercise);
     this.activeModal.close('Close click');
   }
