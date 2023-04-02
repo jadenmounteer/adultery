09706bae731c2143cdd323b16b4298c13d830ad2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-01 22:05:22 -0600

    Kind of make fetch groups method

--- src/app/components/groups-page/groups-page.component.ts
+++ src/app/components/groups-page/groups-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GroupService } from 'src/app/services/groups-service';
 import { AddOrEditGroupModalComponent } from '../add-group-modal/add-or-edit-group-modal.component';
 
 @Component({
@@ -9,9 +10,14 @@ import { AddOrEditGroupModalComponent } from '../add-group-modal/add-or-edit-gro
 })
 export class GroupsPageComponent implements OnInit {
   protected contentLoaded: boolean = true;
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private groupsService: GroupService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.groupsService.fetchGroups();
+  }
 
   protected addNewGroup() {
     const modalRef = this.modalService.open(AddOrEditGroupModalComponent);
--- src/app/services/groups-service.ts
+++ src/app/services/groups-service.ts
@@ -7,7 +7,7 @@ import { Group } from '../types/group';
 import { AuthService } from '../components/auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class ShoppingListService {
+export class GroupService {
   public groups: Array<Group> = [];
   public groupsChanged = new Subject<Group[]>();
 
@@ -16,36 +16,32 @@ export class ShoppingListService {
     private firestore: AngularFirestore
   ) {}
 
-  public fetchGroups() {
+  public async fetchGroups() {
     // Set user object
-    this.authService.user$.subscribe((data) => {
-      console.log(data);
+    this.authService.user$.subscribe((user) => {
+      this.firestore
+        .collection('groups', (ref) => ref.where('users', 'in', user))
+        .snapshotChanges()
+        .pipe(
+          map((docArray: any[]) => {
+            // Here we map the data coming from the db to be the Quote type.
+            return docArray.map((doc) => {
+              if (!doc.payload.doc.data().id) {
+                this.addPayloadIdToGroup(doc.payload.doc.id);
+              }
+              return {
+                id: doc.payload.doc.id,
+                groupName: doc.payload.doc.data().groupName,
+                users: doc.payload.doc.data().users,
+              };
+            });
+          })
+        )
+        .subscribe((groups: Group[]) => {
+          this.groups = groups;
+          this.groupsChanged.next([...this.groups]);
+        });
     });
-    // const userId = this.authService.userId;
-    // this.firestore
-    //   .collection('groups', (ref) => ref.where('userId', 'in', userId))
-    //   .snapshotChanges()
-    //   .pipe(
-    //     map((docArray: any[]) => {
-    //       // Here we map the data coming from the db to be the Quote type.
-    //       return docArray.map((doc) => {
-    //         if (!doc.payload.doc.data().id) {
-    //           this.addPayloadIdToGroup(doc.payload.doc.id);
-    //         }
-    //         return {
-    //           id: doc.payload.doc.id,
-    //           userId: doc.payload.doc.data().userId,
-    //           listName: doc.payload.doc.data().listName,
-    //           items: doc.payload.doc.data().items,
-    //           complete: doc.payload.doc.data().complete,
-    //         };
-    //       });
-    //     })
-    //   )
-    //   .subscribe((groups: Group[]) => {
-    //     this.shopingLists = shopingLists;
-    //     this.shoppingListsChanged.next([...this.shopingLists]);
-    //   });
   }
 
   public addNewGroup(newGroup: Group) {
